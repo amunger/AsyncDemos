83e285bb2614889393bd54a953546f5acca43235
Aaron Munger <aaron.munger@gmail.com>
2020-02-19 19:45:45 -0800

    added demo solution

---
+++ AsyncExamples/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncExamples
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Delay_Click(object sender, RoutedEventArgs e)
+        {
+            SyncOverAsync(4000);
+        }
+
+        private void SyncOverAsync(int milliseconds)
+        {
+            // not going all the way with async
+            DelayAsync(milliseconds).Wait();
+        }
+
+        private async Task DelayAsync(int milliseconds)
+        {
+            //await Task.Delay(milliseconds);
+            await Task.Delay(milliseconds).ConfigureAwait(false);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var count = int.Parse(Count.Text) + 1;
+            Count.Text = count.ToString();
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boot up");
+            Worker.DoWork();
+            Console.WriteLine("Shut down");
+            Console.ReadLine();
+        }
+    }
+
+    public static class Worker
+    {
+        public static void DoWork()
+        {
+            Console.WriteLine("Starting Work");
+            WriteSomeFiles();
+            Console.WriteLine("Finished Work");
+        }
+
+        public static void WriteSomeFiles()
+        {
+            Directory.CreateDirectory("c:/temp");
+            for (int i = 0; i< 1_000; i++)
+            {
+                string path = $"c:/temp/file{i}.txt";
+                using (StreamWriter sw = new StreamWriter(new FileStream(path, FileMode.OpenOrCreate)))
+                {
+                    sw.WriteLine("line");
+                    sw.Flush();
+                }
+            }
+        }
+    }
+
+}
